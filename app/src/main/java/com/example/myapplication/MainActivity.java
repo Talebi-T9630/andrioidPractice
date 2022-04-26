8ce4a1b1c08f13a181e068c97a159a8044a6cdea
Talebi-T9630 <talebitarane@gmail.com>
2022-04-26 00:42:33 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.Console;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtview;
+    EditText edittxt;
+    Button button;
+    String nameInput;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       txtview=(TextView) findViewById(R.id.txtview);
+       edittxt=(EditText) findViewById(R.id.edittxt);
+
+
+        button=(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("button",edittxt.getText().toString());
+                txtview.setText(edittxt.getText().toString());
+            }
+        });
+    }
+}
